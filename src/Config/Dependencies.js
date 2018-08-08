2b80887762083532c130883049f8330c454fc742
Adam Clark <a.clark@comicrelief.com>
2018-08-08 16:46:35 +0100

    Add SQS Service to Dependency Definitions

--- src/Config/Dependencies.js
+++ src/Config/Dependencies.js
@@ -1,14 +1,17 @@
 import LoggerService from '../Service/Logger.service';
 import RequestService from '../Service/Request.service';
+import SQSService from '../Service/SQS.service';
 
 export const DEFINITIONS = {
   LOGGER: 'LOGGER',
   REQUEST: 'REQUEST',
+  SQS: 'SQS',
 };
 
 export const DEPENDENCIES = {
   [DEFINITIONS.LOGGER]: LoggerService,
   [DEFINITIONS.REQUEST]: RequestService,
+  [DEFINITIONS.SQS]: SQSService,
 };
 
 export default {
