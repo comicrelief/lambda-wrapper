02e614b3c18f139aa773d4185f5e7fa22d4391b9
Sidney Barrah <neothedeveloper231@gmail.com>
2019-07-03 16:08:49 +0100

    fix: email is null bug

--- src/Model/SQS/MarketingPreference.model.js
+++ src/Model/SQS/MarketingPreference.model.js
@@ -465,7 +465,7 @@ export default class MarketingPreference extends Model {
       if ((this.getPermissionEmail() !== null
         && this.getPermissionEmail() !== ''
         && this.getPermissionEmail() !== '0')
-      || this.getEmail() !== '') {
+      || (this.getEmail())) {
         if (this.getEmail()) {
           requestConstraintsClone.email = { email: true };
         } else {
