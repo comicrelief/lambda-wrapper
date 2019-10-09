b187b6fea4bd17e579b98b25990a53352b38a106
Mohamed Labib <lebaz20@gmail.com>
2019-10-09 18:33:53 +0200

    Fix unit test

--- tests/unit/Wrapper/LambdaWrapper.js
+++ tests/unit/Wrapper/LambdaWrapper.js
@@ -1,59 +1,64 @@
 import ServerlessMochaPlugin from 'serverless-mocha-plugin';
 import RequestService, {REQUEST_TYPES} from '../../../src/Service/Request.service';
 import DependencyInjection from "../../../src/DependencyInjection/DependencyInjection.class";
-import LambdaWrapper from "../../../src/Wrapper/LambdaWrapper";
+import { LambdaWrapper, LambdaWrapperV2 } from "../../../src/Wrapper/LambdaWrapper";
 
 const expect = ServerlessMochaPlugin.chai.expect;
 
 let getEvent = require('../../mocks/aws/event.json');
 let getContext = require('../../mocks/aws/context.json');
 
-describe('Wrapper/LambdaWrapper', () => {
+const tests = [{ type: 'LambdaWrapper', func: LambdaWrapper }, { type: 'LambdaWrapperV2', func: LambdaWrapperV2 }]
+tests.map(({ type, func }) => (
+  describe(`Wrapper/${type}`, () => {
 
-  let dependencyInjection = {};
-  let requestService = {};
+    let dependencyInjection = {};
+    let requestService = {};
 
-  let configuration = {
-    DEFINITIONS: {},
-    DEPENDENCIES: {},
-  };
+    let configuration = {
+      DEFINITIONS: {},
+      DEPENDENCIES: {},
+    };
 
-  describe('should inject dependency injection into the function', () => {
+    describe('should inject dependency injection into the function', () => {
 
-    LambdaWrapper(configuration, (di, request) => {
-      dependencyInjection = di;
-      requestService = request;
-    })(getEvent, getContext);
+      func(configuration, (di, request) => {
+        dependencyInjection = di;
+        requestService = request;
+        return Promise.resolve();
+      })(getEvent, getContext);
 
-    it('depndency injection variables should be an instance of the dependency injection class', () => {
-      expect(dependencyInjection instanceof DependencyInjection).to.be.true;
+      it('depndency injection variables should be an instance of the dependency injection class', () => {
+        expect(dependencyInjection instanceof DependencyInjection).to.be.true;
+      });
+
+      it('dependency injection should output the event that was provided to it', () => {
+        expect(dependencyInjection.getEvent()).to.eql(getEvent);
+      });
+
+      it('dependency injection should output the event that was provided to it', () => {
+        expect(dependencyInjection.getContext()).to.eql(getContext);
+      });
+
     });
 
-    it('dependency injection should output the event that was provided to it', () => {
-      expect(dependencyInjection.getEvent()).to.eql(getEvent);
+    describe('should inject the request service into the function', () => {
+
+      func(configuration, (di, request) => {
+        dependencyInjection = di;
+        requestService = request;
+        return Promise.resolve();
+      })(getEvent, getContext);
+
+      it('request service variables should be an instance of the dependency injection class', () => {
+        expect(requestService instanceof RequestService).to.be.true;
+      });
+
+      it('request service should contain variables that were sent to it via the event', () => {
+        expect(requestService.get('test'), null, REQUEST_TYPES.GET).to.eql(getEvent.queryStringParameters.test);
+      });
+
     });
-
-    it('dependency injection should output the event that was provided to it', () => {
-      expect(dependencyInjection.getContext()).to.eql(getContext);
-    });
-
-  });
-
-  describe('should inject the request service into the function', () => {
-
-    LambdaWrapper(configuration, (di, request) => {
-      dependencyInjection = di;
-      requestService = request;
-    })(getEvent, getContext);
-
-    it('request service variables should be an instance of the dependency injection class', () => {
-      expect(requestService instanceof RequestService).to.be.true;
-    });
-
-    it('request service should contain variables that were sent to it via the event', () => {
-      expect(requestService.get('test'), null, REQUEST_TYPES.GET).to.eql(getEvent.queryStringParameters.test);
-    });
-
-  });
-});
+  })
+));
 
