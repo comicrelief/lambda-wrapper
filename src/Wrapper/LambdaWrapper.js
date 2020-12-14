11ac8d20bdfdcce4981e0c6265b305d88d8eeac0
William Di Pasquale <wdp@dipasqualew.com>
2020-12-14 12:15:28 +0000

    Handle missing error.response

--- src/Wrapper/LambdaWrapper.js
+++ src/Wrapper/LambdaWrapper.js
@@ -34,11 +34,19 @@ export const handleError = (di, error) => {
       // only keep error.config, error.response.status, error.response.data
       errorBody = {
         config: error.config,
-        response: {
+        message: error.message,
+      };
+
+      // It's pretty common for axios errors
+      // to not have.response e.g.when there's
+      // a network error or timeout.
+      // These errors will have .request but not .response.
+      if (error.response) {
+        errorBody.response = {
           status: error.response.status,
           data: error.response.data,
-        },
-      };
+        };
+      }
     } catch {
       // if the axios error is incomplete
       // for any reason, we don't want
