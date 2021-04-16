c83dd35326d9b1ecade8d302a195d41f98126961
William Di Pasquale <wdp@dipasqualew.com>
2021-04-16 13:38:24 +0100

    feat: Implement HTTP Service
    The HTTP Service will:
    
    1. Wrap axios and perform axios requests via axios.request
    2. Enforce a standard timeout of 10s
    3. Check whether the `x-comicrelief-test-metadata` header is set, in which case the header will be added to downstream requests
    
    See:
    - [ENG-363]

--- src/Config/Dependencies.js
+++ src/Config/Dependencies.js
@@ -1,9 +1,11 @@
+import HTTPService from '../Service/HTTP.service';
 import LoggerService from '../Service/Logger.service';
 import RequestService from '../Service/Request.service';
 import SQSService from '../Service/SQS.service';
 import TimerService from '../Service/Timer.service';
 
 export const DEFINITIONS = {
+  HTTP: 'HTTP',
   LOGGER: 'LOGGER',
   REQUEST: 'REQUEST',
   SQS: 'SQS',
@@ -11,6 +13,7 @@ export const DEFINITIONS = {
 };
 
 export const DEPENDENCIES = {
+  [DEFINITIONS.HTTP]: HTTPService,
   [DEFINITIONS.LOGGER]: LoggerService,
   [DEFINITIONS.REQUEST]: RequestService,
   [DEFINITIONS.SQS]: SQSService,
---
+++ tests/unit/Service/HTTP.service.test.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+
+import CONFIGURATION from '../../../src/Config/Dependencies';
+import DependencyInjection from '../../../src/DependencyInjection/DependencyInjection.class';
+import HTTPService, { COMICRELIEF_TEST_METADATA_HEADER } from '../../../src/Service/HTTP.service';
+
+const getEvent = require('../../mocks/aws/event.json');
+
+const getContext = { invokedFunctionArn: 'my-function' };
+
+const getService = (event = getEvent, context = getContext) => new HTTPService(new DependencyInjection(CONFIGURATION, event, context));
+
+describe('Service/HTTPService', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  describe('request', () => {
+    const testCases = {
+      'GET Request': { method: 'GET', url: '/' },
+      'POST Request': { method: 'POST', url: '/' },
+      'PUT Request': { method: 'PUT', url: '/' },
+      'PATCH Request': { method: 'PATCH', url: '/' },
+      'HEAD Request': { method: 'HEAD', url: '/' },
+      'DELETE Request': { method: 'DELETE', url: '/' },
+      'with URL': { url: '/some/nested/path' },
+      'with baseURL': { baseUrl: 'https://comicrelief.com/test', url: '/additional/url' },
+      'overriding timeout': { timeout: 99 },
+      'with headers': { headers: { Authorization: 'Bearer test' } },
+    };
+
+    Object.entries(testCases).forEach(([description, config]) => {
+      it(description, async () => {
+        const expected = { response: {} };
+        const mock = jest.spyOn(axios, 'request').mockResolvedValue(expected);
+        const service = getService();
+
+        const response = await service.request(config);
+
+        expect(response).toEqual(expected);
+        expect(mock.mock.calls).toMatchSnapshot('config');
+      });
+
+      it(`adds the test header, ${description}`, async () => {
+        const metadata = JSON.stringify({ user: 'Dante Alighieri' });
+        const event = {
+          ...getEvent,
+          headers: {
+            ...getEvent.headers,
+            [COMICRELIEF_TEST_METADATA_HEADER]: metadata,
+          },
+        };
+        const expected = { response: {} };
+        const mock = jest.spyOn(axios, 'request').mockResolvedValue(expected);
+        const service = getService(event);
+
+        const response = await service.request(config);
+
+        expect(response).toEqual(expected);
+        expect(mock.mock.calls).toMatchSnapshot('config');
+      });
+    });
+  });
+});
