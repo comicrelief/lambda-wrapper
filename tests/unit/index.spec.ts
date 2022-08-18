ef4c36e6abfba25c9dea50276b938fc3e8afa539
Seb Aebischer <s.aebischer@comicrelief.com>
2022-08-18 10:48:13 +0100

    Add tests to cover package exports

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,4 +1,11 @@
+import _DependencyAwareClass from '@/src/core/dependency-base';
+import _DependencyInjection from '@/src/core/dependency-injection';
+import _LambdaWrapper from '@/src/core/lambda-wrapper';
+import _SQSService from '@/src/services/SQSService';
+
 import lambdaWrapper, {
+  DependencyAwareClass,
+  DependencyInjection,
   LambdaWrapper,
   SQSService,
 } from '@/src';
@@ -13,5 +20,23 @@ describe('unit.index', () => {
       const deps = Object.values(lambdaWrapper.config.dependencies);
       expect(deps).toContain(SQSService);
     });
+  });
+
+  // these tests prevent accidental removal of exports
+
+  it('should export DependencyAwareClass', () => {
+    expect(DependencyAwareClass).toBe(_DependencyAwareClass);
+  });
+
+  it('should export DependencyInjection', () => {
+    expect(DependencyInjection).toBe(_DependencyInjection);
+  });
+
+  it('should export LambdaWrapper', () => {
+    expect(LambdaWrapper).toBe(_LambdaWrapper);
+  });
+
+  it('should export SQSService', () => {
+    expect(SQSService).toBe(_SQSService);
   });
 });
