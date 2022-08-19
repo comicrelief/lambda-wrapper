7dbe20bd00ea9e9fbfced76cae5a5da8bd608392
Seb Aebischer <s.aebischer@comicrelief.com>
2022-08-19 11:52:01 +0100

    Add JSDoc to SQSService

--- src/services/SQSService.ts
+++ src/services/SQSService.ts
@@ -4,7 +4,36 @@ import DependencyAwareClass from '../core/dependency-base';
 import DependencyInjection from '../core/dependency-injection';
 
 export interface SQSServiceConfig {
+  /**
+   * Maps short friendly queue names to the full SQS queue name.
+   *
+   * Usually we define queue names in our `serverless.yml` and provide them to
+   * the application via environment variables. Example:
+   *
+   * ```ts
+   * {
+   *   queues: {
+   *     submissions: process.env.SQS_QUEUE_SUBMISSIONS,
+   *   }
+   * }
+   * ```
+   */
   queues?: Record<string, string>;
+  /**
+   * Maps short friendly queue names to the queue consumer function name, for
+   * use with offline SQS emulation. Example:
+   *
+   * ```ts
+   * {
+   *   queueConsumers: {
+   *     submissions: 'SubmissionConsumer',
+   *   }
+   * }
+   * ```
+   *
+   * See the [SQSService docs](../../docs/services/SQSService.md) for details
+   * about how this works.
+   */
   queueConsumers?: Record<string, string>;
 }
 
@@ -16,7 +45,31 @@ export interface WithSQSServiceConfig {
  * Helper service for working with SQS.
  *
  * Config for this service goes in the `sqs` key of your Lambda Wrapper config.
- * TODO: more about config
+ * The `queues` key maps short friendly names to the full SQS queue name.
+ * Usually we define queue names in our `serverless.yml` and provide them to
+ * the application via environment variables.
+ *
+ * ```ts
+ * const lambdaWrapper = lw.configure({
+ *   sqs: {
+ *     queues: {
+ *       // add an entry for each queue mapping to its AWS name
+ *       submissions: process.env.SQS_QUEUE_SUBMISSIONS,
+ *     },
+ *   },
+ * });
+ * ```
+ *
+ * You can then send messages to a queue within your Lambda handler using the
+ * `send` method.
+ *
+ * ```ts
+ * export default lambdaWrapper.wrap(async (di) => {
+ *   const sqs = di.get(SQSService);
+ *   const message = { data: 'Hello SQS!' };
+ *   await sqs.send('submissions', message);
+ * });
+ * ```
  */
 export default class SQSService extends DependencyAwareClass {
   private readonly sqs = new SQS({
