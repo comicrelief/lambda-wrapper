c5f3cca6e107537fd7a8e0bac7628f9008e647e1
Seb Aebischer <s.aebischer@comicrelief.com>
2024-03-19 09:37:27 +0000

    Tidy up and cover all methods that take a queue

--- tests/type/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expectTypeOf } from 'expect-type';
-
-it('should pass', () => {
-  expectTypeOf<string>().toEqualTypeOf<string>();
-});
-
-it('should fail', () => {
-  expectTypeOf<string>().not.toEqualTypeOf<number>();
-});
